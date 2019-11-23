4783be1ee65ca79f2a652f33ec71f6afa58a0129
Maisie <maisiesadler@hotmail.com>
2019-11-23 15:08:24 +0000

    trigger pipeline

--- getsentence/main.go
+++ getsentence/main.go
@@ -66,7 +66,7 @@ func getConnectionString() string {
 
 func connect() []byte {
 	connectionString := getConnectionString() // getEnvironmentVariable("connectionstring")
-	fmt.Printf("Got connection string. len=%v\n", len(connectionString))
+	fmt.Printf("Got connection string: len=%v\n", len(connectionString))
 
 	databaseCtx := context.Background()
 
