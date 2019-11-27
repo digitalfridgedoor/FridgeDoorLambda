e54f1aa076cdd99796f803dd7cb35f49476090a6
Maisie <maisiesadler@hotmail.com>
2019-11-27 19:49:30 +0000

    t

--- createrecipe/main.go
+++ createrecipe/main.go
@@ -23,7 +23,7 @@ var errBadRequest = errors.New("Bad request")
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
 	// stdout and stderr are sent to AWS CloudWatch Logs
-	log.Printf("Processing Lambda request  %s\n", request.RequestContext.RequestID)
+	log.Printf("Processing a new Lambda request  %s\n", request.RequestContext.RequestID)
 
 	// If no name is provided in the HTTP request body, throw an error
 	name, ok := request.PathParameters["name"]
