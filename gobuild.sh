2607727b623c30d32e61814554ef4c06b24145bb
Maisie <maisiesadler@hotmail.com>
2019-12-02 13:34:33 +0000

    singular ingredient

--- gobuild.sh
+++ gobuild.sh
@@ -1,6 +1,6 @@
 mkdir bin
 
-go build -o bin/ingredients/get ./functions/ingredients/get
+go build -o bin/ingredient/get ./functions/ingredient/get
 go build -o bin/recipe/get ./functions/recipe/get
 go build -o bin/recipe/p_id/get ./functions/recipe/p_id/get
 go build -o bin/recipe/post ./functions/recipe/post
--- govet_gotest.sh
+++ govet_gotest.sh
@@ -1,10 +1,10 @@
-go vet ./functions/ingredients/get
+go vet ./functions/ingredient/get
 go vet ./functions/recipe/get
 go vet ./functions/recipe/p_id/get
 go vet ./functions/recipe/post
 go vet ./functions/recipe/put
 
-go test ./functions/ingredients/get
+go test ./functions/ingredient/get
 go test ./functions/recipe/get
 go test ./functions/recipe/p_id/get
 go test ./functions/recipe/post
