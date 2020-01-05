3ea7bc9208115e532f3cf0145666b00b88821fe2
Maisie <maisiesadler@hotmail.com>
2020-01-05 19:44:07 +0000

    trigger

--- functions/userview/tags/get/main.go
+++ functions/userview/tags/get/main.go
@@ -24,7 +24,7 @@ var errParseResult = errors.New("Result cannot be parsed")
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
 	// stdout and stderr are sent to AWS CloudWatch Logs
-	log.Printf("Processing Lambda request  ViewUserViews %s\n", request.RequestContext.RequestID)
+	log.Printf("Processing Lambda request ViewUserViews %s\n", request.RequestContext.RequestID)
 
 	view, err := userviewapi.GetOrCreateUserView(context.Background(), &request)
 	if err != nil {
