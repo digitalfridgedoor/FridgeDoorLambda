890aa155b53d82e550bc2e75782b5efa52993001
Maisie <maisiesadler@hotmail.com>
2021-08-01 19:03:54 +0100

    info logging

--- functions_js/code/src/index.ts
+++ functions_js/code/src/index.ts
@@ -4,6 +4,9 @@ exports.handler = async function (event, context, callback) {
     console.log('Hello, world');
 
     const message = event.Records[0].Sns.Message;
+    console.log(event.Records[0].Sns);
+    console.log(message);
+    console.log('getting html from ' + message.url);
     const html = await getHtml(message.url)
     console.log(html)
 };
