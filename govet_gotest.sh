8d81a4b8cdf22c2ef30c131ff9926f3c05560bba
maisiesadler <maisiesadler@hotmail.com>
2022-10-11 19:42:21 +0100

    remove clipping

--- govet_gotest.sh
+++ govet_gotest.sh
@@ -1,8 +1,3 @@
-go vet ./functions/clipping/p_id/delete
-go vet ./functions/clipping/p_id/get
-go vet ./functions/clipping/post
-go vet ./functions/clipping/put
-go vet ./functions/clipping/search/get
 go vet ./functions/image/get
 go vet ./functions/ingredient/get
 go vet ./functions/ingredient/put
@@ -16,11 +11,6 @@ go vet ./functions/recipe/put
 go vet ./functions/userview/get
 go vet ./functions/userview/tags/get
 
-go test ./functions/clipping/p_id/delete
-go test ./functions/clipping/p_id/get
-go test ./functions/clipping/post
-go test ./functions/clipping/put
-go test ./functions/clipping/search/get
 go test ./functions/image/get
 go test ./functions/ingredient/get
 go test ./functions/ingredient/put
